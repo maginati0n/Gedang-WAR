cfa26bc7cfb1c8814f0e0843c36b7e3153654a8d
Dimas Tri Sasongko <dimas.sasongko001@binus.ac.id>
2022-09-28 11:21:18 +0700

    Initial Commit

---
+++ script/index.js
@@ -0,0 +1,12 @@
+var pepaya = 0;
+
+function myPepaya() {
+  pepaya += 1;
+  document.getElementById("pepaya").innerHTML = pepaya;
+};
+
+var pisang = 0;
+function myPisang() {
+    pisang += 1;
+    document.getElementById("pisang").innerHTML = pisang;
+};
